74430d0eeaa4044e2427f02af387906bbbd511a7
mariasannino <maria.sannino01@estudiant.upf.edu>
2025-03-27 13:09:59 +0100

    assignment2

--- basics-assignment-2-start/assignment.js
+++ basics-assignment-2-start/assignment.js
@@ -1,1 +1,22 @@
-const task3Element = document.getElementById('task-3');
+const task3Element = document.getElementById("task-3");
+
+function someAlert() {
+  alert("Hey! : )");
+}
+
+function nameAlert(name) {
+  alert("Hey! " + name);
+}
+
+someAlert();
+nameAlert("Maria");
+
+task3Element.addEventListener("click", someAlert);
+
+function strings(str1, str2, str3) {
+  const result = `${str1} ${str2}  ${str3} `;
+  return result;
+}
+
+const result = strings("Hello", "Maria", "!");
+alert(result);
