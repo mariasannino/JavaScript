f25c13f780e918633d134268e83d30135beed0af
mariasannino <maria.sannino01@estudiant.upf.edu>
2025-03-27 11:45:23 +0100

    basics

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,12 @@
+const defaultResult = 0;
+let currentResult;
+currentResult = defaultResult;
+
+currentResult = (currentResult + 10) * 3;
+
+/* let calculationDescription = "(0 +10)*3"; */
+/* let calculationDescription = "(" + defaultResult + "+10)*3"; */
+let calculationDescription = `(  ${defaultResult}  +10)*3`;
+
+let errorMessage = "An error" + "occurred!";
+outputResult(currentResult, errorMessage);
