bf2333ce90d5aa99b8a9a08793b57c0995422541
mariasannino <maria.sannino01@estudiant.upf.edu>
2025-03-27 14:34:11 +0100

    section2_complete

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,14 +1,67 @@
 const defaultResult = 0;
 let currentResult;
 currentResult = defaultResult;
+let logEntries = [];
 /* let result; defining globally */
+
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+function createAndWriteOutput(operator, resultBefore, calcNum) {
+  const calcDescription = `${resultBefore}  ${operator}  ${calcNum}`;
+  outputResult(currentResult, calcDescription);
+}
+function writeTolog(operationId, prevResult, operationNum, newResult) {
+  const logEntry = {
+    operation: operationId,
+    prevResult: prevResult,
+    number: operationNum,
+    result: newResult,
+  }; /* create an object */
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
 function add() {
-  currentResult = currentResult + userInput.value;
-  outputResult(currentResult, "");
+  const enteredNum = parseInt(userInput.value);
+  const initialResult = currentResult;
+  currentResult += enteredNum;
+  /* also with + +userInput.value */ /* .tostring() */
+  createAndWriteOutput("+", initialResult, enteredNum);
   /* alert("The result is: \n" + result); */
+  /*  logEntries.push(logEntry);  push new element add new element aray 
+  console.log(logEntry.operation);  will return only the operation 
+  console.log(logEntries);*/
+  writeTolog("ADD", initialResult, enteredNum, currentResult);
 }
 
+function subtract() {
+  const enteredNum = parseInt(userInput.value);
+  const initialResult = currentResult;
+  currentResult -= enteredNum;
+  //currentResult++ adds one --, ** if we put it before the variable we will get the updated value if not the before value
+  createAndWriteOutput("-", initialResult, enteredNum);
+  writeTolog("SUBSTRACT", initialResult, enteredNum, currentResult);
+}
+
+function multiply() {
+  const enteredNum = parseInt(userInput.value);
+  const initialResult = currentResult;
+  currentResult *= enteredNum;
+  createAndWriteOutput("*", initialResult, enteredNum);
+  writeTolog("MULTIPLY", initialResult, enteredNum, currentResult);
+}
+
+function divide() {
+  const enteredNum = parseInt(userInput.value);
+  const initialResult = currentResult;
+  currentResult /= enteredNum;
+  createAndWriteOutput("/", initialResult, enteredNum);
+  writeTolog("DIVIDE", initialResult, enteredNum, currentResult);
+}
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
 /* when button click have a look at the add function and execute it for me */
 
 /* currentResult = (currentResult + 10) * 3; */
