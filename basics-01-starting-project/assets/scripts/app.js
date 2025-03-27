f01417d4455f98ed953267b38ecb14e4a6314395
mariasannino <maria.sannino01@estudiant.upf.edu>
2025-03-27 12:40:38 +0100

    functcont

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,12 +1,20 @@
 const defaultResult = 0;
 let currentResult;
 currentResult = defaultResult;
+/* let result; defining globally */
+function add() {
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, "");
+  /* alert("The result is: \n" + result); */
+}
 
-currentResult = (currentResult + 10) * 3;
+addBtn.addEventListener("click", add);
+/* when button click have a look at the add function and execute it for me */
 
+/* currentResult = (currentResult + 10) * 3; */
+/* currentResult = add(1, 2); */
 /* let calculationDescription = "(0 +10)*3"; */
 /* let calculationDescription = "(" + defaultResult + "+10)*3"; */
-let calculationDescription = `(  ${defaultResult}  +10)*3`;
+/* let calculationDescription = `(  ${defaultResult}  +10)*3`;
 
-let errorMessage = "An error" + "occurred!";
-outputResult(currentResult, calculationDescriptionS);
+let errorMessage = "An error" + "occurred!"; */
