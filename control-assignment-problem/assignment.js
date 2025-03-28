68c846307bf5fcfb4be4d7e7853cea0cdd7ff652
mariasannino <maria.sannino01@estudiant.upf.edu>
2025-03-28 14:59:37 +0100

    third_assginment

--- control-assignment-problem/assignment.js
+++ control-assignment-problem/assignment.js
@@ -1,1 +1,36 @@
 const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
+
+if (randomNumber >= 0.7) {
+  alert("the number is bigger!");
+} else {
+  alert("the num is smaller");
+}
+
+numbers = [0, 1, 2, 3, 4];
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+for (const num of numbers) {
+  console.log(num);
+}
+
+let j = 0;
+while (j < numbers.lenght) {
+  console.log(numbers[j]);
+  j++;
+}
+
+for (let i = numbers.length - 1; i >= 0; i--) {
+  console.log(numbers[i]);
+}
+
+const randNumber2 = Math.random();
+if (
+  (randomNumber > 0.7 && randNumber2 > 0.7) ||
+  randNumber2 <= 0.2 ||
+  randomNumber <= 0.2
+) {
+  alert("a lot of conditions!");
+}
