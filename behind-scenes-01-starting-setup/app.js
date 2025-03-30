fed3de29667869c8904b7d4f9e8e7a26575cc698
mariasannino <maria.sannino01@estudiant.upf.edu>
2025-03-30 14:04:01 +0200

    section5

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,31 @@
+/* let name = "Maria";
+let hobbies;
+
+{
+  let test = 5;
+  console.log(test); /only used inside the brackets 
+}
+if (name === "Maria") {
+  hobbies = ["Art", "Cooking"];
+  console.log(
+    hobbies
+  );  as it is not a function var (hobbies is like a global variable now. If we use let or const we get an error. In modern java we use only let and const
+}
+
+function greet() {
+  var age = 22;
+  var name = "Mariana";
+  console.log(name, age, hobbies);
+}
+
+console.log(name, hobbies);
+greet(); */
+
+/* console.log(userName);
+var userName =
+  "Maria"; you will get an undifined variable but not an error. Wit let or const you will get an error. */
+
+"use strict";
+const userName = "Maria";
+var undefined = 5;
+console.log(userName);
