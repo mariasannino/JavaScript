39f3bd3193755c68e6e18b319fc5575677e4f92b
mariasannino <maria.sannino01@estudiant.upf.edu>
2025-03-31 13:50:20 +0200

    section7

---
+++ dom-01-starting-code/app.js
@@ -0,0 +1,16 @@
+const h1 = document.getElementById("main-title");
+h1.textContent = "Some new title!";
+h1.style.color = "white";
+h1.style.backgroundColor = "black";
+
+const li = document.querySelector("li:last-of-type");
+li.textContent = li.textContent + "(changed!)";
+
+const body = document.body;
+/* body.querySelector; */
+
+/* const listItemElements = document.querySelectorAll("li"); */
+const listItemElements = document.getElementsByTagName("li");
+for (const listItemEL of listItemElements) {
+  console.dir(listItemEL);
+}
