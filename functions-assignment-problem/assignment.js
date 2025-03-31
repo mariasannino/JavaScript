a902c7469e9aaf404e6a0626832060a3a6539e2f
mariasannino <maria.sannino01@estudiant.upf.edu>
2025-03-31 11:50:22 +0200

    assignment done

--- functions-assignment-problem/assignment.js
+++ functions-assignment-problem/assignment.js
@@ -1,5 +1,49 @@
-function sayHello(name) {
-  console.log('Hi ' + name);
-}
+const sayHello = (name) => {
+  console.log("Hi " + name);
+};
 
-sayHello();
+/* const sayHello = name => console.log("Hi" + name) */
+
+sayHello("Maria");
+
+const sayHello2 = (hello, name) => {
+  console.log(hello + name);
+};
+
+sayHello2("HI ", "MARIA");
+
+const sayHello3 = () => {
+  const hello = "Hello ";
+  const name = "Maria!";
+  console.log(hello + name);
+};
+
+sayHello3();
+
+const sayHello4 = (name) => "Hola " + name;
+
+console.log(sayHello4("Maria"));
+
+const DEFAULT_VALUE = "Mari";
+const sayHello5 = (name = DEFAULT_VALUE) => {
+  console.log("Hi " + name);
+};
+
+sayHello5();
+sayHello5("Marins");
+
+const checkInput = (resultHandler, ...strings) => {
+  const validateString = (str) => {
+    return !str ? "something " : str;
+  };
+  let sum = "So ";
+  for (const string of strings) {
+    sum += validateString(string);
+  }
+  resultHandler(sum);
+};
+const showResult = (result) => {
+  console.log(result);
+};
+checkInput(showResult, "hi ", "my ", "name ", "is ", "maria ");
+checkInput(showResult, "hi ", 0, "name ", "is ", "maria ");
