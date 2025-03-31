45ec4fd0560778ad8c75c3749cb443e2b240fc2b
mariasannino <maria.sannino01@estudiant.upf.edu>
2025-03-31 15:17:17 +0200

    section7

--- dom-01-starting-code/app.js
+++ dom-01-starting-code/app.js
@@ -1,4 +1,6 @@
-const h1 = document.getElementById("main-title");
+/* FIRST PART */
+
+/* const h1 = document.getElementById("main-title");
 h1.textContent = "Some new title!";
 h1.style.color = "white";
 h1.style.backgroundColor = "black";
@@ -7,10 +9,31 @@ const li = document.querySelector("li:last-of-type");
 li.textContent = li.textContent + "(changed!)";
 
 const body = document.body;
-/* body.querySelector; */
+ body.querySelector; 
 
-/* const listItemElements = document.querySelectorAll("li"); */
+ const listItemElements = document.querySelectorAll("li"); 
+
 const listItemElements = document.getElementsByTagName("li");
 for (const listItemEL of listItemElements) {
   console.dir(listItemEL);
-}
+} */
+
+/* SECOND PART: PARENT CHILD SIBLINGS */
+const ul = document.body.firstElementChild.nextElementSibling;
+const firstLi = ul.firstElementChild;
+console.log(firstLi);
+
+const section = document.querySelector("section ");
+const button = document.querySelector("button");
+/* section.style.backgroundColor = "blue"; */
+section.className = "red-bg";
+button.addEventListener("click", () => {
+  /*   if (section.className === "red-bg visible") {
+    section.className = "red-bg invisible";
+  } else {
+    section.className = "red-bg visible";
+  } */
+
+  /*   section.classList.toggle("visible"); */
+  section.classList.toggle("invisible");
+});
