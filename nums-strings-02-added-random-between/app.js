b0442f09a41ef81c56db17fe719a732ad3913102
mariasannino <maria.sannino01@estudiant.upf.edu>
2025-04-07 10:36:49 +0200

    section16done

--- nums-strings-02-added-random-between/app.js
+++ nums-strings-02-added-random-between/app.js
@@ -4,3 +4,22 @@ function randomIntBetween(min, max) {
 }
 
 console.log(randomIntBetween(1, 10));
+
+function productDescription(strings, productName, productPrice) {
+  console.log(strings);
+  console.log(productName);
+  console.log(productPrice);
+  let priceCategory = "cheap";
+  if (productPrice > 10) {
+    priceCategory = "fair";
+  }
+
+  /*  return `${strings[0]}${productName}${strings[1]}${priceCategory}${strings[2]}`; */
+  return { name: productName, price: productPrice };
+}
+
+const prodName = "teapot";
+const prodPrice = 15.99;
+
+const productOutput = productDescription`This product(${prodName}) is ${prodPrice}.`;
+console.log(productOutput);
